edabe94ae7d90e1342ace63bc20135ac3b300051
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2026-03-07 01:24:53 -0300

    Adicionando mais arquivos

---
+++ src/main/java/br/com/tiagopitombeira/Array.java
@@ -0,0 +1,13 @@
+package br.com.tiagopitombeira;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		
+		int[] numero = {10,20,30,40};
+		
+		System.out.println(numero[0]);
+		
+	}
+	
+}
---
+++ src/main/java/br/com/tiagopitombeira/PecorrendoArray.java
@@ -0,0 +1,17 @@
+package br.com.tiagopitombeira;
+
+public class PecorrendoArray {
+	
+	public static void main(String[] args) {
+		
+		int[] numero = {10,20,30,40};
+		
+		for(int i = 0;i < numero.length;i++) {
+			
+			System.out.println(numero[i]);
+			
+		}
+		
+	}
+	
+}
